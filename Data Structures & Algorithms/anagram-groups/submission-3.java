1c061bda0ead4b16c3dc06841f36bf53876eb1cf
Sravanthi96 <sravanthi.1054@gmail.com>
2026-07-16 18:08:13 -0700

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<List<String>>();
+        Map<Integer,String> exists = new HashMap<Integer,String>();
+        for(int i =0 ; i < strs.length;i++) {
+            List<String> resultList = new ArrayList<String>();
+            if(!exists.containsKey(i)){
+                resultList.add(strs[i]);
+            }
+            for(int j= i+1; j < strs.length;j++) {
+                if(!exists.containsKey(j) && isMatch(strs[i], strs[j])){
+                    resultList.add(strs[j]);
+                    exists.put(i,strs[i]);
+                    exists.put(j,strs[j]);
+                }
+            }
+            if(!resultList.isEmpty())
+                result.add(resultList);
+        }
+        return result;
+    }
+
+    private boolean isMatch(String s , String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+        int[] freq = new int[26];
+        for(char c : s.toCharArray()) {
+            freq[c-'a']++;
+        }
+        for(char c : t.toCharArray()) {
+            freq[c-'a']--;
+        }
+        for(int count : freq) {
+            if(count>0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
