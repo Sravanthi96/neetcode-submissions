9d740e4b84253eb81f7eb6be89b5fd9e10669d6b
Sravanthi96 <sravanthi.1054@gmail.com>
2026-08-03 12:46:25 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String copy = s.replaceAll("\\s+","");
+        String str = copy.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        char[] strArray = str.toCharArray();
+        System.out.println("str" + str);
+        int l =0;
+        int r = strArray.length-1;
+        while(l<r) {
+            if(str.charAt(l) == str.charAt(r)) {
+                l++;
+                r--;
+            }else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
